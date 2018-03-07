9a853747e94c809af4c17faea0575b1cc856200b
Robert M <robertm@kainos.com>
2018-03-07 12:07:28 +0000

    Working json for basic custom docker image deployment! Yay!

--- modules/wso2/main.tf
+++ modules/wso2/main.tf
@@ -4,101 +4,70 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_template_deployment" "wso2-app-service" {
-    name = "wso2_template_01",
-    resource_group_name = "${var.rg_name}"
-    template_body = <<DEPLOY
-    {
-    "parameters": {
-        "name": {
-            "type": "string",
-             "defaultvalue": "${var.wso2_name}"
-        },
-        "servicePlanName": {
-            "type": "string",
-            "defaultvalue": "[concat('wso2',uniqueString(resourceGroup().id))]"
-        },
-        "location": {
-            "type": "string",
-            "defaultvalue": "${var.rg_location}"
-        },
-        "hostingEnvironment": {
-            "type": "string",
-            "defaultvalue": ""
-        },
-        "serverFarmResourceGroup": {
-            "type": "string",
-            "defaultvalue": "${var.rg_name}"
-        },
-        "sku" : {
-            "type": "string",
-            "defaultvalue": "${var.app_service_sku}"
-        },
-        "skuCode" : {
-            "type": "string",
-            "defaultvalue": "${var.app_service_sku_code}"
-        },
-        "workerSize" : {
-            "type": "string",
-            "defaultvalue": "${var.app_service_worker_size}"
-        }
-    },
-    "resources": [
-        {
-            "apiVersion": "2016-08-01",
-            "kind": "app,linux,container",
-            "name": "[parameters('name')]",
-            "type": "Microsoft.Web/Sites",
-            "properties": {
-                "name": "[parameters('name')]",
-                "siteConfig": {
-                    "alwaysOn": true,
-                    "appSettings": [
-                        {
-                            "WEBSITES_ENABLE_APP_SERVICE_STORAGE": "false",
-                            "DATABASE_USER": "${var.database_user}",
-                            "DATABASE_PASSWORD": "${var.database_password}",
-                            "DATABASE_URL": "${var.database_url}",
-                            "CARBON_PROTOCOL": "${var.carbon_protocol}",
-                            "CARBON_PORT": "${var.carbon_port}",
-                            "CARBON_HOST": "${var.carbon_host}",
-                            "LPG_UI_URL": "${var.lpg_ui_url}",
-                            "WEBSITES_PORT": "${var.carbon_port}"
-                        }
-                    ],
-                    "linuxFxVersion": "DOCKER|${var.docker_image}:${var.docker_tag}"
-                },
-                "serverFarmId": "[resourceId('Microsoft.Web/serverfarms', parameters('servicePlanName'))]"
-            },
-            "dependsOn": [
-                "[resourceId('Microsoft.Web/serverfarms', parameters('servicePlanName'))]"
-            ],
-            "location": "[resourceGroup().location]",
-            "tags": {
-                "[concat('hidden-related:', '/subscriptions/', '/resourcegroups/', parameters('serverFarmResourceGroup'), '/providers/Microsoft.Web/serverfarms/', parameters('servicePlanName'))]": "empty"
-            }
-        },
-        {
-            "apiVersion": "2016-09-01",
-            "name": "[parameters('servicePlanName')]",
-            "type": "Microsoft.Web/serverfarms",
-            "location": "[parameters('location')]",
-            "properties": {
-                "name": "[parameters('servicePlanName')]",
-                "workerSizeId": "[parameters('workerSize')]",
-                "reserved": true,
-                "numberOfWorkers": "1",
-                "hostingEnvironment": "[parameters('hostingEnvironment')]"
-            },
-            "sku": {
-                "Tier": "[parameters('sku')]",
-                "Name": "[parameters('skuCode')]"
-            },
-            "kind": "linux"
-        }
-    ],
-    "$schema": "http://schema.management.azure.com/schemas/2014-04-01-preview/deploymentTemplate.json#",
-    "contentVersion": "1.0.0.0"
-    }
-    DEPLOY
-    deployment_mode = "Incremental"
+  name = "${var.wso2_name}"
+  resource_group_name = "${var.rg_name}"
+  template_body = <<DEPLOY
+  {
+      "$schema": "http://schema.management.azure.com/schemas/2014-04-01-preview/deploymentTemplate.json#",
+      "contentVersion": "1.0.0.0",
+      "parameters": {
+          "siteName": {
+              "type": "string",
+              "defaultvalue": "${var.wso2_name}",
+              "metadata": {
+                  "description": "Name of azure web app"
+              }
+          }
+      },
+      "variables": {
+          "hostingPlanName": "[concat(parameters('siteName'), 'serviceplan')]"
+      },
+      "resources": [
+          {
+              "type": "Microsoft.Web/sites",
+              "name": "[parameters('siteName')]",
+              "properties": {
+                  "siteConfig": {
+                      "appSettings": [
+                          {
+                              "name": "DOCKER_CUSTOM_IMAGE_NAME",
+                              "value": ""
+                          },
+                          {
+
+                          }
+                      ]
+                  },
+                  "name": "[parameters('siteName')]",
+                  "serverFarmId": "[variables('hostingPlanName')]",
+                  "hostingEnvironment": ""
+              },
+              "apiVersion": "2016-03-01",
+              "location": "[resourceGroup().location]",
+              "dependsOn": [
+                  "[variables('hostingPlanName')]"
+              ]
+          },
+          {
+              "apiVersion": "2016-09-01",
+              "name": "[variables('hostingPlanName')]",
+              "type": "Microsoft.Web/serverfarms",
+              "location": "[resourceGroup().location]",
+              "properties": {
+                  "name": "[variables('hostingPlanName')]",
+                  "workerSizeId": "1",
+                  "reserved": true,
+                  "numberOfWorkers": "1",
+                  "hostingEnvironment": ""
+              },
+              "sku": {
+                  "Tier": "Standard",
+                  "Name": "S1"
+              },
+              "kind": "linux"
+          }
+      ]
+  }
+  DEPLOY
+  deployment_mode = "Incremental"
 }
