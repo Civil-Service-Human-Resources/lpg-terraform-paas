7c1d43cc97347daaa5aa845895763a16cb06e147
Pritpal Panesar <36885087+pritpalp@users.noreply.github.com>
2018-03-08 12:02:05 +0000

    lpg-learner-record container and turned on docker logging

---
+++ modules/lpg-learner-record/vars.tf
@@ -0,0 +1,36 @@
+variable "lpg_learner_record_name" {
+    default = "name"
+}
+variable "rg_name" {
+  default = "holder"
+}
+variable "rg_prefix" {
+  default = "rgpref"
+}
+variable "rg_location" {
+  default = "location"
+}
+variable "docker_tag" {
+  default = "docker_tag"
+}
+variable "docker_image" {
+  default = "cshr/lpg-learner-record"
+}
+variable "websites_port" {
+  default = "9000"
+}
+variable "auth_user" {
+  default = "user"
+}
+variable "auth_password" {
+  default = "password"
+}
+variable "xapi_url" {
+  default = "xapi_url"
+}
+variable "xapi_username" {
+  default = "66f2b4fc001e3da992d23b57d8a7457655bea078"
+}
+variable "xapi_password" {
+  default = "1c0e1b6827606d7efed71e204939d048f94f842b"
+}
