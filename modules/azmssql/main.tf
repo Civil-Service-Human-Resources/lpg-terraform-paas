2636dc136eb98f455d69287f2bebd299b7c405ca
Robert M <robertm@kainos.com>
2018-03-08 14:13:16 +0000

    Adding Azure MS SQL Database configuration to replace postgres

---
+++ modules/azmssql/main.tf
@@ -0,0 +1,33 @@
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.rg_name}"
+  location = "${var.rg_location}"
+}
+
+resource "azurerm_sql_server" "sqlserv" {
+    name                         = "${var.sql_serv_name}"
+    resource_group_name          = "${var.rg_name}"
+    location                     = "${var.rg_location}"
+    version                      = "12.0"
+    administrator_login          = "${var.sql_admin}"
+    administrator_login_password = "${var.sql_pass}"
+}
+
+resource "azurerm_sql_database" "sqldb" {
+  name                = "${var.sql_db_name}"
+  resource_group_name = "${var.rg_name}"
+  location            = "${var.rg_location}"
+  server_name         = "${var.sql_serv_name}"
+
+  tags {
+    environment = "${var.environment_tag}"
+  }
+}
+
+resource "azurerm_sql_firewall_rule" "sqlfwrule" {
+  name                = "${var.sql_rule_name}"
+  resource_group_name = "${var.rg_name}"
+  server_name         = "${var.sql_serv_name}"
+  start_ip_address    = "${var.start_ip}"
+  end_ip_address      = "${var.end_ip}"
+}
---
+++ modules/azmssql/vars.tf
@@ -0,0 +1,33 @@
+variable "rg_name" {
+  default = "holder"
+}
+variable "rg_prefix" {
+  default = "rgpref"
+}
+variable "rg_location" {
+  default = "location"
+}
+variable "sql_serv_name" {
+  default = "sqlname"
+}
+variable "sql_db_name" {
+  default = "sqldb"
+}
+variable "sql_admin" {
+  default = "sqladmin"
+}
+variable "sql_pass" {
+  default = "replaceme!@18"
+}
+variable "environment_tag" {
+  default = "unknown"
+}
+variable "sql_rule_name" {
+  default = "firewall-rule-"
+}
+variable "start_ip" {
+  default = "0.0.0.0/32"
+}
+variable "end_ip" {
+  default = "0.0.0.0/32"
+}
