767af3e31369ecfe064683af186ae75cdf30b595
Pritpal Panesar <36885087+pritpalp@users.noreply.github.com>
2018-03-09 14:07:33 +0000

    Learning locker admin api server container

---
+++ modules/learning-locker-api-server/main.tf
@@ -0,0 +1,112 @@
+###### learning-locker-api-server ######
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.rg_name}"
+  location = "${var.rg_location}"
+}
+
+resource "azurerm_template_deployment" "learning-locker-api-server-app-service" {
+  name = "${var.learning_locker_api_server_name}"
+  resource_group_name = "${var.rg_name}"
+  template_body = <<DEPLOY
+  {
+      "$schema": "http://schema.management.azure.com/schemas/2014-04-01-preview/deploymentTemplate.json#",
+      "contentVersion": "1.0.0.0",
+      "parameters": {
+          "siteName": {
+              "type": "string",
+              "defaultvalue": "${var.learning_locker_api_server_name}",
+              "metadata": {
+                  "description": "Name of azure web app"
+              }
+          }
+      },
+      "variables": {
+          "hostingPlanName": "[concat(parameters('siteName'), 'serviceplan')]"
+      },
+      "resources": [
+          {
+              "type": "Microsoft.Web/sites",
+              "name": "[parameters('siteName')]",
+              "properties": {
+                  "siteConfig": {
+                      "appSettings": [
+                          {
+                              "name": "DOCKER_CUSTOM_IMAGE_NAME",
+                              "value": "${var.docker_image}:${var.docker_tag}"
+                          },
+                          {
+                              "name": "WEBSITES_ENABLE_APP_SERVICE_STORAGE",
+                              "value": "false"
+                          },
+                          {
+                              "name": "MONGO_URL",
+                              "value": "${var.mongo_url}"
+                          },
+                          {
+                              "name": "MONGODB_PATH",
+                              "value": "${var.mongodb_path}"
+                          },
+                          {
+                              "name": "REDIS_HOST",
+                              "value": "${var.redis_host}"
+                          },
+                          {
+                              "name": "REDIS_URL",
+                              "value": "${var.redis_url}"
+                          },
+                          {
+                              "name": "WEBSITES_PORT",
+                              "value": "${var.websites_port}"
+                          }
+                      ]
+                  },
+                  "name": "[parameters('siteName')]",
+                  "serverFarmId": "[variables('hostingPlanName')]",
+                  "hostingEnvironment": ""
+              },
+              "apiVersion": "2016-03-01",
+              "location": "[resourceGroup().location]",
+              "dependsOn": [
+                  "[variables('hostingPlanName')]"
+              ]
+          },
+          {
+              "apiVersion": "2016-09-01",
+              "name": "[variables('hostingPlanName')]",
+              "type": "Microsoft.Web/serverfarms",
+              "location": "[resourceGroup().location]",
+              "properties": {
+                  "name": "[variables('hostingPlanName')]",
+                  "workerSizeId": "1",
+                  "reserved": true,
+                  "numberOfWorkers": "1",
+                  "hostingEnvironment": ""
+              },
+              "sku": {
+                  "Tier": "Standard",
+                  "Name": "S1"
+              },
+              "kind": "linux"
+          },
+          {
+            "type": "Microsoft.Web/sites/config",
+            "name": "[concat(parameters('siteName'), '/web')]",
+            "apiVersion": "2016-08-01",
+            "location": "UK South",
+            "scale": null,
+            "properties": {
+                "httpLoggingEnabled": true,
+                "logsDirectorySizeLimit": 35,
+                "detailedErrorLoggingEnabled": true,
+                "appCommandLine": "/opt/learning-locker/./node_modules/.bin/babel-node /opt/learning-locker/mkadmin.js"
+            },
+            "dependsOn": [
+                "[resourceId('Microsoft.Web/sites', parameters('siteName'))]"
+            ]
+        }
+      ]
+  }
+  DEPLOY
+  deployment_mode = "Incremental"
+}
