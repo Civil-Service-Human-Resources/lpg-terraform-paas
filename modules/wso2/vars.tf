ac7175ea5572f9daa4e6747c1643140aa35137b2
Pritpal Panesar <36885087+pritpalp@users.noreply.github.com>
2018-03-15 14:25:30 +0000

    Various little changes including wso2 db_url change

--- modules/wso2/vars.tf
+++ modules/wso2/vars.tf
@@ -15,55 +15,23 @@ variable "rg_location" {
 }
 
 variable "docker_tag" {
-  default = "docker_tag"
+  default = "latest"
 }
 
 variable "docker_image" {
   default = "docker_image"
 }
 
-variable "database_user" {
-  default = "db_user"
-}
-
 variable "database_url" {
   default = "db_url"
-}
-
-variable "database_password" {
-  default = "db_pwd"
 }
 
 variable "carbon_protocol" {
   default = "http"
 }
 
-variable "carbon_port" {
-  default = "3000"
-}
-
-variable "carbon_host" {
-  default = "carbon_host"
-}
-
-variable "lpg_ui_url" {
-  default = "lqg_ui_url"
-}
-
 variable "websites_port" {
   default = "9443"
-}
-
-variable "app_service_sku" {
-  default = "Standard"
-}
-
-variable "app_service_sku_code" {
-  default = "S1"
-}
-
-variable "app_service_worker_size" {
-  default = "1"
 }
 
 variable "environment_tag" {
