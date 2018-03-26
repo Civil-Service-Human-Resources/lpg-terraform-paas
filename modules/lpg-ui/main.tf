23694813044c1db8db086379174b4ef6e2bfb1a0
Pritpal Panesar <36885087+pritpalp@users.noreply.github.com>
2018-03-26 17:57:18 +0100

    ui hammer changes

--- modules/lpg-ui/main.tf
+++ modules/lpg-ui/main.tf
@@ -95,6 +95,18 @@ resource "azurerm_template_deployment" "lpg-ui-app-service" {
                           {
                               "name": "YOUTUBE_API_KEY",
                               "value": "${var.youtube_api_key}"
+                          },
+                          {
+                              "name": "HAMMER_LOGSTASH_HOST",
+                              "value": "${var.hammer_logstash_host}"
+                          },
+                          {
+                              "name": "HAMMER_LOGSTASH_PORT",
+                              "value": "${var.hammer_logstash_port}"
+                          },
+                          {
+                              "name": "ENV_PROFILE",
+                              "value": "${var.env_profile}"
                           }
                       ]
                   },
--- modules/lpg-ui/vars.tf
+++ modules/lpg-ui/vars.tf
@@ -81,3 +81,15 @@ variable "xapi_url" {
 variable "youtube_api_key" {
   default = "youtube_api_key"
 }
+
+variable "hammer_logstash_host" {
+  default = "54e2fb5d-be7a-47c2-b3cf-6f72f42b5dfb-ls.logit.io"
+}
+
+variable "hammer_logstash_port" {
+  default = "16690"
+}
+
+variable "env_profile" {
+  default = "test"
+}
