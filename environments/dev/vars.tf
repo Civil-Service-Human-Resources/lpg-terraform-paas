a91da70a47afc06d039d90159cf8df4722dc967c
Pritpal Panesar <36885087+pritpalp@users.noreply.github.com>
2018-04-13 14:46:27 +0100

    Adding dev and fixing learninglocker script

---
+++ environments/dev/vars.tf
@@ -0,0 +1,236 @@
+### Gen vars ###
+variable "rg_name" {
+  type    = "string"
+  default = "lpgdev"
+}
+
+variable "rg_prefix" {
+  type    = "string"
+  default = "lpg"
+}
+
+variable "rg_location" {
+  type    = "string"
+  default = "UK South"
+}
+
+variable "authentication_service_url" {
+  default = "https://dev-identity.cshr.digital"
+}
+
+variable "lpg_ui_url" {
+  default = "https://dev-lpg.cshr.digital"
+}
+
+variable "lpg_ui_server" {
+  default = "dev-lpg.cshr.digital"
+}
+
+variable "lpg_management_ui_url" {
+  default = "https://dev-admin.cshr.digital"
+}
+
+variable "hammer_api_worker_http_server_port" {
+  default = "8080"
+}
+
+variable "hammer_logstash_host" {
+  default = "54e2fb5d-be7a-47c2-b3cf-6f72f42b5dfb-ls.logit.io"
+}
+
+variable "hammer_logstash_port" {
+  default = "16690"
+}
+
+variable "env_profile" {
+  default = "dev"
+}
+
+variable "youtube_api_key" {
+  default = "AIzaSyDkapeVQajSishsrZTDmqi4rLRRq3HABrY"
+}
+
+variable "gov_notify_api_key" {
+  default = "test-22877568-4bca-40f9-8f54-8bf7922583cf-e2f80f22-e3de-4007-9ce2-b50a2b5785b7"
+}
+
+variable "google_analytics_id" {
+  default = "UA-22141655-3"
+}
+
+variable "elasticsearch_uri" {
+  default = "https://demo-elastic.cshr.digital:9200"
+}
+
+variable "content_url" {
+  default = "http://local-cdn.cshr.digital/lpgdevcontent"
+}
+
+variable "envurl" {
+  default = "dev-"
+}
+
+### docker tags ###
+variable "wso2_docker_tag" {
+  default = "0a3168c832991fd4575a35ba816a417ecc154eb2-v2"
+}
+
+variable "lpg_learner_record_docker_tag" {
+  default = "0a44bda01502cfa1cfc603a79b99da2620830d58-v2"
+}
+
+variable "ll_docker_tag" {
+  default = "fe8104bf2f0fedb8f5af138bd8860a9c27dd2bb12ecb827f09e754e279a70fa8"
+}
+
+variable "lpg_services_tag" {
+  default = "latest"
+}
+
+variable "learning_catalogue_docker_tag" {
+  default = "latest"
+}
+
+### postgres ###
+variable "postgres_name" {
+  default = "postgres"
+}
+
+variable "postgres_sku_name" {
+  default = "PGSQLB50"
+}
+
+variable "postgres_sku_capacity" {
+  default = 50
+}
+
+variable "postgres_sku_tier" {
+  default = "Basic"
+}
+
+### cosmos ###
+variable "cosmos_name" {
+  default = "cosmos"
+}
+
+variable "cosmos_offer_type" {
+  default = "Standard"
+}
+
+variable "cosmos_consistency_policy_level" {
+  default = "Strong"
+}
+
+variable "cosmos_failover_policy_location" {
+  default = "West Europe"
+}
+
+### redis ###
+variable "redis_queue_provider" {
+  default = "REDIS"
+}
+
+variable "redis_queue_namespace" {
+  default = "learninglocker"
+}
+
+variable "redis_prefix" {
+  default = "learninglocker"
+}
+
+variable "redis_use_tls" {
+  default = "1"
+}
+
+### wso2 ###
+variable "wso2_name" {
+  default = "wso2"
+}
+
+variable "wso2_docker_image" {
+  default = "cshr/lpg-wso2-is"
+}
+
+### lpg-learner-record ###
+variable "lpg_learner_record_name" {
+  default = "lpg-learner-record"
+}
+
+variable "xapi_url" {
+  default = "xapi_url"
+}
+
+variable "lpg_learner_record_websites_port" {
+  default = "9000"
+}
+
+### learning-locker-general ###
+variable "lpg_testing" {
+  default = "1"
+}
+
+### learning-locker-xapi ###
+variable "lpg_learning_locker_xapi_name" {
+  default = "learning-locker-xapi"
+}
+
+variable "ll_xapi_express_port" {
+  default = "8083"
+}
+
+### learning-locker-worker ###
+variable "lpg_learning_locker_worker_name" {
+  default = "learning-locker-api-worker"
+}
+
+variable "worker_api_env_app" {
+  default = "learning-locker-api-worker"
+}
+
+variable "worker_api_hammer_directory" {
+  default = "/opt/learning-locker"
+}
+
+### learning-locker-ui ###
+variable "lpg_learning_locker_ui_name" {
+  default = "learning-locker-ui"
+}
+
+variable "lpg_learning_locker_api_port" {
+  default = "8082"
+}
+
+variable "ll_ui_port" {
+  default = "80"
+}
+
+### lpg-management ###
+variable "lpg_management_name" {
+  default = "lpg-management-ui"
+}
+
+variable "management_ui_hammer_working_directory" {
+  default = "/var/www/app/dist/management-ui"
+}
+
+variable "management_ui_websites_port" {
+  default = "3001"
+}
+
+### lpg-ui ###
+variable "lpg_ui_name" {
+  default = "lpg-ui"
+}
+
+variable "ui_hammer_working_directory" {
+  default = "/var/www/app/dist/ui"
+}
+
+variable "ui_websites_port" {
+  default = "3001"
+}
+
+### lpg-learning-catalogue ###
+variable "lpg_learning_catalogue_name" {
+  default = "lpg-learning-catalogue"
+}
