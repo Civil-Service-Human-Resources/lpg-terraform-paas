f0249dd2207a08a845b06803c53eaf585308a59c
Pritpal Panesar <36885087+pritpalp@users.noreply.github.com>
2018-05-03 16:07:26 +0100

    Updated script to change values in the webapps using the password

--- scripts/management/redis_password.sh
+++ scripts/management/redis_password.sh
@@ -1,8 +1,9 @@
-#!/bin/bash -ex
+#!/bin/bash -ev
 
 echo "This script will check the redis password for non URL safe characters and regenerate if any are found."
 echo "You need to have logged in via Azure CommandLine (az-cli)"
 echo "Parameters needed are: ResourceGroup, name of Redis cache"
+echo "Example: ./redis_password.sh lpgtest lpg-lpgtest-redis"
 
 key=$(az redis list-keys -g $1 -n $2 -o tsv| awk -F ' ' '{print $1}')
 
@@ -12,6 +13,13 @@ if [[ "$key" =~ [^0-9A-Za-z\=]+ ]] ; then
   do
     key=$(az redis regenerate-keys --key-type Primary -g $1 -n $2 -o tsv| awk -F ' ' '{print $1}')
   done
+  apiworker=$(az webapp config appsettings set -g $1 -n lpg-$1-learning-locker-api-worker --settings REDIS_PASSWORD=$key)
+  xapi=$(az webapp config appsettings set -g $1 -n lpg-$1-learning-locker-xapi --settings REDIS_URL=redis://$2.redis.cache.windows.net:6380/0?password=$key)
+  echo "Primary Redis key is now: $key"
+  echo "This password is used in the following places, please check they have been updated:"
+  echo " * learning-locker-api-worker - REDIS_PASSWORD"
+  echo " * learning-locker-xapi - REDIS_URL"
 else
-  echo "Password is ok"
+  echo "Password is ok, nothing done"
 fi
+
