2daf7bc09776d801592281608d423fc616eb716e
Pritpal Panesar <36885087+pritpalp@users.noreply.github.com>
2018-05-03 16:25:17 +0100

    Clean up of scripts

--- scripts/management/update_image.sh
+++ scripts/management/update_image.sh
@@ -2,8 +2,8 @@
 
 echo "This script will update the Docker image for a Azure Webapp for Container"
 echo "Ensure that you've already logged in to Azure via the CLI"
-echo "Please pass the webapp container name, resource group and docker tag (in order)"
-
+echo "Please pass the webapp name, resource group and docker tag (in order)"
+echo "Example: ./update_images.sh lpg-lpgdev-learning-locker-api-worker lpgdev cshr/lpg-learning-locker:213ad45eb11f7924fcec151e852bb729476279338f30ffb040daa3f2df896f5c-v2"
 if [[ $# -eq 0 ]]; then
   echo "You've forgotten the arguments, or supplied too few, please try again"
   exit 1
