b65829795220a2bdde4fd1045aeb18e097a0b220
Pritpal Panesar <36885087+pritpalp@users.noreply.github.com>
2018-05-04 16:22:01 +0100

    Wrapper script for add-admin-user.sh

---
+++ scripts/learninglocker/run_me.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -ev
+
+# This script wraps the add-admin-user.sh and provides it with all variables it needs from the environment
+# As always, assumption that you've logged in via Azure CLI - and that its installed
+# Also docker is required to be running
+#
+# Required variables: resource group
+# Example: ./run_me.sh lpgtest
+
+# apikey=$(az webapp config appsettings set -g $1 -n lpg-$1-lpg-learer-record --settings REDIS_PASSWORD=$key)
+
+xapikey=$(az webapp config appsettings list -g $1 -n lpg-$1-lpg-learner-record --query "[?name=='XAPI_USERNAME'].[value]" -o table | sed -n '3p')
+xapivalue=$(az webapp config appsettings list -g $1 -n lpg-$1-lpg-learner-record --query "[?name=='XAPI_PASSWORD'].[value]" -o table | sed -n '3p')
+lockeruser=$(az webapp config appsettings list -g $1 -n lpg-$1-lpg-learner-record --query "[?name=='AUTH_USER'].[value]" -o table | sed -n '3p')
+lockerpass=$(az webapp config appsettings list -g $1 -n lpg-$1-lpg-learner-record --query "[?name=='AUTH_PASSWORD'].[value]" -o table | sed -n '3p')
+mongopath=$(az webapp config appsettings list -g $1 -n lpg-$1-learning-locker-ui --query "[?name=='MONGODB_PATH'].[value]" -o table | sed -n '3p')
+dockertag=$(az webapp config container show -g $1 -n lpg-$1-learning-locker-ui -o table --query "[?name=='DOCKER_CUSTOM_IMAGE_NAME'].[value]" | sed -n '3p' | sed 's/\DOCKER\|cshr\/lpg-learning-locker\://g')
+path=$(pwd)
+(exec $path/add-admin-user.sh $lockeruser $lockerpass $xapikey $xapivalue \"$mongopath\" $dockertag)
