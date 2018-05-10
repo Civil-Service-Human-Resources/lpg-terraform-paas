b7bb916ca7866ec159a3518645bb6d4efb39ca97
Pritpal Panesar <36885087+pritpalp@users.noreply.github.com>
2018-05-10 11:00:28 +0100

    MySQL module

---
+++ modules/mysql/vars.tf
@@ -0,0 +1,24 @@
+variable "rg_name" {
+  default = "holder"
+}
+variable "rg_prefix" {
+  default = "rgpref"
+}
+variable "rg_location" {
+  default = "location"
+}
+variable "mysql_location" {
+  default = "West Europe"
+}
+variable "mysql_name" {
+  default = "mysql"
+}
+variable "env_profile" {
+  default = ""
+}
+variable "mysql_admin_login" {
+  default = ""
+}
+variable "mysql_admin_pass" {
+  default = ""
+}
