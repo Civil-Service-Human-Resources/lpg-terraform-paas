7210d8c0ac7ce9de74b4a4dc5be7c09031ba54ac
Rob Marks <robertm@kainos.com>
2018-05-21 12:03:17 +0100

    Why: * Need to set the character set for DB
    This change addresses the need by:
    * Adds a line to update the character set to UTF8
    
    Changes made:
    * Add line to update character set to UTF8

--- scripts/db/mysql_properties.sh
+++ scripts/db/mysql_properties.sh
@@ -11,6 +11,7 @@
 timezone=$(az mysql server configuration set -g $1 -s $2 -n time_zone --value +00:00)
 slowlog=$(az mysql server configuration set -g $1 -s $2 -n slow_query_log --value on)
 queriesnotusingindex=$(az mysql server configuration set -g $1 -s $2 -n log_queries_not_using_indexes --value on)
+charset=$(az mysql server configuration set -g $1 -s $2 -n character_set_server --value UTF8)
 
 up=$(az webapp config appsettings list -g lpgdev -n lpg-lpgdev-identity -o tsv| grep -i datasource | awk -F ' ' '{print $3}'| cut -d? -f2| awk -F '&' '{print $1} {print $2}' | cut -d= -f2)
 read -a arr <<< $up
