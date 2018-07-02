a8757b53be420d5ee9a2ac79eaf73cc42097d102
Rob Marks <robertm@kainos.com>
2018-07-02 15:17:28 +0100

    Create Applicaton Insights Module
    Why:
    * Ping and things
    
    This change addresses the need by:
    * This is still a work in progress module
    
    Changes made:
    * Added ping for lpgui & identity
    
    To be done:
    * Any other pings
    * Metric monitoring

---
+++ modules/application-insights/main.tf
@@ -0,0 +1,219 @@
+###### app insights ######
+
+resource "azurerm_template_deployment" "app-insights" {
+  name = "app-insights"
+  resource_group_name = "${var.rg_name}"
+
+  template_body = <<DEPLOY
+{
+    "$schema": "http://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#",
+    "contentVersion": "1.0.0.0",
+    "parameters": {
+        "appName": {
+            "type": "string",
+            "defaultValue": "${var.envurl}app-insights-1"
+        },
+        "appType": {
+            "type": "string",
+            "defaultValue": "other"
+        },
+        "appLocation": {
+            "type": "string",
+            "defaultValue": "West Europe"
+        },
+        "pingUrl-lpgUI": {
+            "type": "string",
+            "defaultValue": "https://${var.envurl}lpg.cshr.digital"
+        },
+        "pingText-lpgUI": {
+            "type": "string",
+            "defaultValue": "lpgUI"
+        },
+            "pingUrl-identity": {
+            "type": "string",
+            "defaultValue": "https://${var.envurl}identity.cshr.digital"
+        },
+        "pingText-identity": {
+            "type": "string",
+            "defaultValue": "identity"
+        }
+    },
+    "variables": {
+        "pingTestName-lpgUI": "[concat('PingTest-', parameters('pingText-lpgUI'))]",
+        "pingAlertName-lpgUI": "[concat('PingAlert-', parameters('pingText-lpgUI'), '-', subscription().subscriptionId)]",
+        "pingTestName-identity": "[concat('PingTest-', parameters('pingText-identity'))]",
+        "pingAlertName-identity": "[concat('PingAlert-', parameters('pingText-identity'), '-', subscription().subscriptionId)]"
+    },
+    "resources": [
+        {
+            "type": "microsoft.insights/components",
+            "kind": "[parameters('appType')]",
+            "name": "[parameters('appName')]",
+            "apiVersion": "2014-04-01",
+            "location": "[parameters('appLocation')]",
+            "tags": {},
+            "properties": {
+                "ApplicationId": "[parameters('appName')]"
+            },
+            "dependsOn": []
+        },
+        {
+            "name": "[variables('pingTestName-lpgUI')]",
+            "type": "Microsoft.Insights/webtests",
+            "apiVersion": "2014-04-01",
+            "location": "[parameters('appLocation')]",
+            "dependsOn": [
+            "[resourceId('Microsoft.Insights/components', parameters('appName'))]"
+            ],
+            "tags": {
+                "[concat('hidden-link:', resourceId('Microsoft.Insights/components', parameters('appName')))]": "Resource"
+            },
+            "properties": {
+                "Name": "[variables('pingTestName-lpgUI')]",
+                "Description": "Basic ping test",
+                "Enabled": true,
+                "Frequency": 300,
+                "Timeout": 120,
+                "Kind": "ping",
+                "RetryEnabled": true,
+                "Locations": [
+                {
+                    "Id": "emea-nl-ams-azr"
+                },
+                {
+                    "Id": "emea-ru-msa-edge"
+                },
+                {
+                    "Id": "emea-se-sto-edge"
+                },
+                {
+                    "Id": "emea-gb-db3-azr"
+                }
+                ],
+                "Configuration": {
+                "WebTest": "[concat('<WebTest   Name=\"', variables('pingTestName-lpgUI'), '\"   Enabled=\"True\"         CssProjectStructure=\"\"    CssIteration=\"\"  Timeout=\"120\"  WorkItemIds=\"\"         xmlns=\"http://microsoft.com/schemas/VisualStudio/TeamTest/2010\"         Description=\"\"  CredentialUserName=\"\"  CredentialPassword=\"\"         PreAuthenticate=\"True\"  Proxy=\"default\"  StopOnError=\"False\"         RecordedResultFile=\"\"  ResultsLocale=\"\">  <Items>  <Request Method=\"GET\"    Version=\"1.1\"  Url=\"', parameters('pingUrl-lpgUI'),   '\" ThinkTime=\"0\"  Timeout=\"300\" ParseDependentRequests=\"True\"         FollowRedirects=\"True\" RecordResult=\"True\" Cache=\"False\"         ResponseTimeGoal=\"0\"  Encoding=\"utf-8\"  ExpectedHttpStatusCode=\"200\"         ExpectedResponseUrl=\"\" ReportingName=\"\" IgnoreHttpStatusCode=\"False\" />        </Items>    </WebTest>')]"
+                },
+                "SyntheticMonitorId": "[variables('pingTestName-lpgUI')]"
+            }
+        },
+        {
+            "name": "[variables('pingTestName-identity')]",
+            "type": "Microsoft.Insights/webtests",
+            "apiVersion": "2014-04-01",
+            "location": "[parameters('appLocation')]",
+            "dependsOn": [
+            "[resourceId('Microsoft.Insights/components', parameters('appName'))]"
+            ],
+            "tags": {
+                "[concat('hidden-link:', resourceId('Microsoft.Insights/components', parameters('appName')))]": "Resource"
+            },
+            "properties": {
+                "Name": "[variables('pingTestName-identity')]",
+                "Description": "Basic ping test",
+                "Enabled": true,
+                "Frequency": 300,
+                "Timeout": 120,
+                "Kind": "ping",
+                "RetryEnabled": true,
+                "Locations": [
+                {
+                    "Id": "emea-nl-ams-azr"
+                },
+                {
+                    "Id": "emea-ru-msa-edge"
+                },
+                {
+                    "Id": "emea-se-sto-edge"
+                },
+                {
+                    "Id": "emea-gb-db3-azr"
+                }
+                ],
+                "Configuration": {
+                "WebTest": "[concat('<WebTest   Name=\"', variables('pingTestName-identity'), '\"   Enabled=\"True\"         CssProjectStructure=\"\"    CssIteration=\"\"  Timeout=\"120\"  WorkItemIds=\"\"         xmlns=\"http://microsoft.com/schemas/VisualStudio/TeamTest/2010\"         Description=\"\"  CredentialUserName=\"\"  CredentialPassword=\"\"         PreAuthenticate=\"True\"  Proxy=\"default\"  StopOnError=\"False\"         RecordedResultFile=\"\"  ResultsLocale=\"\">  <Items>  <Request Method=\"GET\"    Version=\"1.1\"  Url=\"', parameters('pingUrl-identity'),   '\" ThinkTime=\"0\"  Timeout=\"300\" ParseDependentRequests=\"True\"         FollowRedirects=\"True\" RecordResult=\"True\" Cache=\"False\"         ResponseTimeGoal=\"0\"  Encoding=\"utf-8\"  ExpectedHttpStatusCode=\"200\"         ExpectedResponseUrl=\"\" ReportingName=\"\" IgnoreHttpStatusCode=\"False\" />        </Items>    </WebTest>')]"
+                },
+                "SyntheticMonitorId": "[variables('pingTestName-identity')]"
+            }
+        },
+        {
+            "name": "[variables('pingAlertName-lpgUI')]",
+            "type": "Microsoft.Insights/alertrules",
+            "apiVersion": "2014-04-01",
+            "location": "[parameters('appLocation')]",
+            "dependsOn": [
+                "[resourceId('Microsoft.Insights/webtests', variables('pingTestName-lpgUI'))]"
+            ],
+            "tags": {
+                "[concat('hidden-link:', resourceId('Microsoft.Insights/components', parameters('appName')))]": "Resource",
+                "[concat('hidden-link:', resourceId('Microsoft.Insights/webtests', variables('pingTestName-lpgUI')))]": "Resource"
+            },
+            "properties": {
+                "name": "[variables('pingAlertName-lpgUI')]",
+                "description": "alert for web test",
+                "isEnabled": true,
+                "condition": {
+                    "$type": "Microsoft.WindowsAzure.Management.Monitoring.Alerts.Models.LocationThresholdRuleCondition, Microsoft.WindowsAzure.Management.Mon.Client",
+                    "odata.type": "Microsoft.Azure.Management.Insights.Models.LocationThresholdRuleCondition",
+                    "dataSource": {
+                        "$type": "Microsoft.WindowsAzure.Management.Monitoring.Alerts.Models.RuleMetricDataSource, Microsoft.WindowsAzure.Management.Mon.Client",
+                        "odata.type": "Microsoft.Azure.Management.Insights.Models.RuleMetricDataSource",
+                        "resourceUri": "[resourceId('microsoft.insights/webtests', variables('pingTestName-lpgUI'))]",
+                        "metricName": "GSMT_AvRaW"
+                    },
+                    "windowSize": "PT5M",
+                    "failedLocationCount": 3
+                },
+                "actions": [
+                    {
+                        "$type": "Microsoft.WindowsAzure.Management.Monitoring.Alerts.Models.RuleEmailAction, Microsoft.WindowsAzure.Management.Mon.Client",
+                        "odata.type": "Microsoft.Azure.Management.Insights.Models.RuleEmailAction",
+                        "sendToServiceOwners": false,
+                        "customEmails": []
+                    }
+                ]
+            }
+        },
+        {
+            "name": "[variables('pingAlertName-identity')]",
+            "type": "Microsoft.Insights/alertrules",
+            "apiVersion": "2014-04-01",
+            "location": "[parameters('appLocation')]",
+            "dependsOn": [
+                "[resourceId('Microsoft.Insights/webtests', variables('pingTestName-identity'))]"
+            ],
+            "tags": {
+                "[concat('hidden-link:', resourceId('Microsoft.Insights/components', parameters('appName')))]": "Resource",
+                "[concat('hidden-link:', resourceId('Microsoft.Insights/webtests', variables('pingTestName-identity')))]": "Resource"
+            },
+            "properties": {
+                "name": "[variables('pingAlertName-identity')]",
+                "description": "alert for web test",
+                "isEnabled": true,
+                "condition": {
+                    "$type": "Microsoft.WindowsAzure.Management.Monitoring.Alerts.Models.LocationThresholdRuleCondition, Microsoft.WindowsAzure.Management.Mon.Client",
+                    "odata.type": "Microsoft.Azure.Management.Insights.Models.LocationThresholdRuleCondition",
+                    "dataSource": {
+                        "$type": "Microsoft.WindowsAzure.Management.Monitoring.Alerts.Models.RuleMetricDataSource, Microsoft.WindowsAzure.Management.Mon.Client",
+                        "odata.type": "Microsoft.Azure.Management.Insights.Models.RuleMetricDataSource",
+                        "resourceUri": "[resourceId('microsoft.insights/webtests', variables('pingTestName-identity'))]",
+                        "metricName": "GSMT_AvRaW"
+                    },
+                    "windowSize": "PT5M",
+                    "failedLocationCount": 3
+                },
+                "actions": [
+                    {
+                        "$type": "Microsoft.WindowsAzure.Management.Monitoring.Alerts.Models.RuleEmailAction, Microsoft.WindowsAzure.Management.Mon.Client",
+                        "odata.type": "Microsoft.Azure.Management.Insights.Models.RuleEmailAction",
+                        "sendToServiceOwners": false,
+                        "customEmails": []
+                    }
+                ]
+            }
+        }
+    ]
+}
+  DEPLOY
+
+  deployment_mode = "Incremental"
+}
