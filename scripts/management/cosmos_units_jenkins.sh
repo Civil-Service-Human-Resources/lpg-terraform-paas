47f8f9f2c73c0409b0ed85c7c9ed870e01ca8247
Rob Marks <robertm@kainos.com>
2018-08-01 14:18:41 +0100

    LPFG-624 - New script that will scale the RU units of all collections/dbs - mainly intended for Jenkins

---
+++ scripts/management/cosmos_units_jenkins.sh
@@ -0,0 +1,80 @@
+#!/bin/bash -e
+
+#######################################
+##
+## This script will update the cosmos units for all cosmos DBs based on time frame
+##
+##
+## Usage:
+## ./cosmos_units_jenkins.sh
+##
+## Created by:       Robert Marks
+## Creation Date:    01/08/2018
+##
+########################################
+
+#az login --service-principal --username APP_ID --password PASSWORD --tenant TENANT_ID
+subs=$(az account list | grep -i \"id\"\: | awk -F ':' '{gsub(/\"|\,/,"")}1 {print $2}')
+read -a subsarr <<< $subs
+
+time=$(date +%H:%M)
+echo $time
+
+sdown=400
+sup=1000
+
+if [[ $time > "06:59" ]] && [[ $time < "18:59" ]]; then
+    echo "Scaling Up!"
+    for s in "${subsarr[@]}"; do
+
+        az account set --subscription "${s}"
+        collection=$(az cosmosdb list | grep -i \"name\"\: | awk -F ':' '{gsub(/\"|\,/,"")}1 {print $2}')
+        read -a col <<< $collection
+
+        for c in "${col[@]}"; do
+            g=$(echo $c | cut -d'-' -f 2)
+            key=$(az cosmosdb list-keys -g ${g} -n ${c} -o tsv | awk -F ' ' '{print $1}')
+            exists=$(az cosmosdb database exists --db-name admin -n ${c} --key "$key")
+
+            if [[ "$exists" == true ]] ; then
+                echo "admin database exists - changing RUs per collection for $c"
+                db=$(az cosmosdb collection list --db-name admin -n ${c} --key "$key" | grep -i \"id\"\: | awk -F ':' '{gsub(/\"|\,/,"")}1 {print $2}')
+                read -a arr <<< $db
+
+                for i in "${arr[@]}"; do
+                    echo "Updating $i RU for $c collection"
+                    az cosmosdb collection update --c "$i" -d admin --throughput "$sup" --key "$key" -n ${c}
+                done
+             else
+                echo "admin database does not exist for $c"
+            fi
+        done
+    done
+else
+    echo "Scaling Down!"
+    for s in "${subsarr[@]}"; do
+
+        az account set --subscription "${s}"
+        collection=$(az cosmosdb list | grep -i \"name\"\: | awk -F ':' '{gsub(/\"|\,/,"")}1 {print $2}')
+        read -a col <<< $collection
+
+        for c in "${col[@]}"; do
+            g=$(echo $c | cut -d'-' -f 2)
+            key=$(az cosmosdb list-keys -g ${g} -n ${c} -o tsv | awk -F ' ' '{print $1}')
+            exists=$(az cosmosdb database exists --db-name admin -n ${c} --key "$key")
+
+            if [[ "$exists" == true ]] ; then
+                echo "admin database exists - changing RUs per collection for $c"
+                db=$(az cosmosdb collection list --db-name admin -n ${c} --key "$key" | grep -i \"id\"\: | awk -F ':' '{gsub(/\"|\,/,"")}1 {print $2}')
+                read -a arr <<< $db
+
+                for i in "${arr[@]}"; do
+                    echo "Updating $i RU for $c collection"
+                    az cosmosdb collection update --c "$i" -d admin --throughput "$down" --key "$key" -n ${c}
+                done
+             else
+                echo "admin database does not exist for $c"
+            fi
+        done
+    done
+fi
