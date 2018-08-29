cac87097e354705db91b73b8a353154999d42819
Rob Marks <robertm@kainos.com>
2018-08-29 21:40:41 +0100

    acr config lpg-management2

--- environments/master/main.tf
+++ environments/master/main.tf
@@ -339,4 +339,7 @@ module "lpg-management2" {
   course_catalogue_url                = "https://${var.envurl}learning-resources.cshr.digital"
   course_catalogue_user               = "${var.course_catalogue_user}"
   course_catalogue_pass               = "${var.course_catalogue_pass}"
+  acr_url                             = ""
+  acr_username                        = ""
+  acr_password                        = ""
 }
