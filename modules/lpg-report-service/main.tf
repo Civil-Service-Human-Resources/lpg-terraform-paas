1238c0d98d01edb2a9cc05c4f52ce50ed71e9cfa
Rob Marks <robertm@kainos.com>
2018-09-07 07:13:28 +0100

    update to docker config for acr

--- modules/lpg-report-service/main.tf
+++ modules/lpg-report-service/main.tf
@@ -206,7 +206,7 @@ resource "azurerm_template_deployment" "lpg-report-service-app-service" {
         "detailedErrorLoggingEnabled": true,
         "alwaysOn": true,
         "appCommandLine": "",
-        "linuxFxVersion": "DOCKER|${var.docker_image}:${var.docker_tag}",
+        "linuxFxVersion": "DOCKER|${var.docker_registry_server_url}/${var.docker_image}:${var.docker_tag}",
         "minTlsVersion": "1.0",
         "ftpsState": "Disabled"
       },
