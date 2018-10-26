633148c774fda8d2b3f982f64c7bee07c8331757
Rob Marks <robertm@kainos.com>
2018-10-27 00:23:54 +0100

    learning catalogue linting and scaling

--- modules/lpg-learning-catalogue/vars.tf
+++ modules/lpg-learning-catalogue/vars.tf
@@ -121,3 +121,11 @@ variable "docker_registry_server_username" {
 variable "docker_registry_server_password" {
   default = ""
 }
+
+variable "custom_emails" {
+  default = ""
+}
+
+variable "scaling_enabled" {
+  default = ""
+}
