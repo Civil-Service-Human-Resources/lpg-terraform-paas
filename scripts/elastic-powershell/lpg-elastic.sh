7e1becca3a507773f386d257aabf0e97fd85bee2
Rob Marks <robertm@kainos.com>
2018-12-13 16:26:23 +0000

    update to elastic deploy script

--- scripts/elastic-powershell/lpg-elastic.sh
+++ scripts/elastic-powershell/lpg-elastic.sh
@@ -1,14 +1,31 @@
 #!/bin/bash -e
 
+# This script will deploy elasticsearch to azure based on the configuration stored in the .json file selected
+#
+# You need to set subID / rgName / env vars
+# For the env var that is the name of the json file you want to use (integration.json / staging.json / production.json)
+# The json files are stored securely in keybase
+#
+# Example: ./lpg-elastic.sh
+
 subID=""
-rgName="lpg-prod-elastic"
-rgLocation="UKSouth"
-deployName="prod-elastic-thurs10"
+rgName=""
+env=""
+rgLocation=""
+deployName=""
 
 az account set --subscription $subID
 
-az group create --location $rgLocation --name $rgName
+exists=$(az group exists -n $rgName)
 
-az group deployment create -g $rgName -n $deployName --template-uri "https://raw.githubusercontent.com/elastic/azure-marketplace/master/src/mainTemplate.json" --parameters @lpg-prod-elastic-parameters.json
+if [[ $exists != true ]] ; then
+    echo "Creating RG"
+    az group create --location $rgLocation --name $rgName
+    echo "Deploying elastic from $env"
+    az group deployment create -g $rgName -n $deployName --template-uri "https://raw.githubusercontent.com/elastic/azure-marketplace/master/src/mainTemplate.json" --parameters @$env
+else
+    echo "Deploying elastic from $env"
+    az group deployment create -g $rgName -n $deployName --template-uri "https://raw.githubusercontent.com/elastic/azure-marketplace/master/src/mainTemplate.json" --parameters @$env
+fi
 
-#az network nsg rule update -g $rgName --nsg-name
+#az network nsg list -g $rgName | grep -i \"name\"\: | awk -F ':' '{gsub(/\"|\,/,"")}1 {print $2}' | awk 'NR==7'
