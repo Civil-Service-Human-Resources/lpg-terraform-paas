df09ee93630341547231340769cee20827fa43a9
Rob Marks <robertm@kainos.com>
2018-12-14 12:52:56 +0000

    update elastic deploy script

--- scripts/elastic-powershell/lpg-elastic.sh
+++ scripts/elastic-powershell/lpg-elastic.sh
@@ -8,16 +8,20 @@
 #
 # Example: ./lpg-elastic.sh
 
+# Fill out relevant details
 subID=""
 rgName=""
 env=""
 rgLocation=""
 deployName=""
 
+# Set the subscription
 az account set --subscription $subID
 
+# Check if RG exists
 exists=$(az group exists -n $rgName)
 
+# Deploy
 if [[ $exists != true ]] ; then
     echo "Creating RG"
     az group create --location $rgLocation --name $rgName
@@ -28,4 +32,6 @@ else
     az group deployment create -g $rgName -n $deployName --template-uri "https://raw.githubusercontent.com/elastic/azure-marketplace/master/src/mainTemplate.json" --parameters @$env
 fi
 
-#az network nsg list -g $rgName | grep -i \"name\"\: | awk -F ':' '{gsub(/\"|\,/,"")}1 {print $2}' | awk 'NR==7'
+# Set SSH to DENY to start
+nsgName=$(az network nsg list -g $rgName | grep -i \"name\"\: | awk -F ':' '{gsub(/\"|\,/,"")}1 {print $2}' | awk 'NR==7')
+az network nsg rule update -g $rgName --nsg-name $nsgName -n SSH --access DENY
