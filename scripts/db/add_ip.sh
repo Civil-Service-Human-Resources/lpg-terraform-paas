ccd79a704523850e3b31b9f947a450399ca859f8
Pritpal <pritpalp@kainos.com>
2019-10-31 10:15:46 +0000

    Added in Redis (for session) firewall setting

--- scripts/db/add_ip.sh
+++ scripts/db/add_ip.sh
@@ -1,4 +1,8 @@
 #!/bin/bash -ev
+##### BROKEN AT THE MOMENT ######
+## If the Redis firewall list is empty the script breaks
+## FIX ME
+
 
 # This script should get a list of all outbound ip's and add them to the mysql instance firewall rules
 # It may take a while (few minutes) for the changes to actually take effect in Azure
@@ -11,14 +15,22 @@
 # Example: ./add_ip.sh lpgtest lpg-lpgtest-wso2
 
 IN=$(az webapp show -g $1 -n $2 --query possibleOutboundIpAddresses -o tsv)
-existing=$(az mysql server firewall-rule list -g $1 -s lpg-$1-mysql | grep -i startipaddress)
+existingMySQL=$(az mysql server firewall-rule list -g $1 -s lpg-$1-mysql | grep -i startipaddress)
+# existingRedis=$(az redis firewall-rules list -g $1 -n lpg-$1-redis-session | grep -i startIp)
 
 ipList=$(echo ${IN} | tr "," "\n")
+az webapp show -g $1 -n $2 --query possibleOutboundIpAddresses -o tsv
+echo ${IN}
+
 for i in ${ipList}
 do
   # do something here
-  if [[ ${existing} != *"${i}"* ]]; then
+  if [[ ${existingMySQL} != *"${i}"* ]]; then
     echo "Adding rule for ${i}"
-    addRule=$(az mysql server firewall-rule create -g $1 -s lpg-$1-mysql --name Rule_${1}_${i//./} --start-ip-address ${i} --end-ip-address ${i})
+    addMySQLRule=$(az mysql server firewall-rule create -g $1 -s lpg-$1-gp --name Rule_${1}_${i//./} --start-ip-address ${i} --end-ip-address ${i})
   fi
+  # if [[ ${existingRedis} != *"${i}"* ]]; then
+    echo "Adding rule to Redis for ${i}"
+    addRedisRule=$(az redis firewall-rules create -g $1 -n lpg-$1-redis-session --rule-name Rule_${1}_${i//./} --start-ip ${i} --end-ip ${i})
+  # fi
 done
