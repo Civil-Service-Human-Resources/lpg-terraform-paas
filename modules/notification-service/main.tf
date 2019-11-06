bcde9d1e19466ec72314fb06ece394925456f2ff
Rob Marks <robertm@kainos.com>
2019-11-06 10:43:04 +0000

    Notification-Service new template

--- modules/notification-service/main.tf
+++ modules/notification-service/main.tf
@@ -6,7 +6,7 @@ resource "azurerm_template_deployment" "notification-service-app-service" {
 
   template_body = <<DEPLOY
   {
-     "$schema":"http://schema.management.azure.com/schemas/2014-04-01-preview/deploymentTemplate.json#",
+     "$schema":"https://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#",
      "contentVersion":"1.0.0.0",
      "parameters":{
         "siteName":{
@@ -88,9 +88,8 @@ resource "azurerm_template_deployment" "notification-service-app-service" {
               "httpsOnly":true,
               "name":"[parameters('siteName')]",
               "serverFarmId":"[variables('hostingPlanName')]",
-              "hostingEnvironment":""
            },
-           "apiVersion":"2016-03-01",
+           "apiVersion":"2019-08-01",
            "location":"[resourceGroup().location]",
            "tags":{
               "environment":"${var.env_profile}"
@@ -100,38 +99,36 @@ resource "azurerm_template_deployment" "notification-service-app-service" {
            ]
         },
         {
-           "apiVersion":"2016-09-01",
+           "apiVersion":"2019-08-01",
            "name":"[variables('hostingPlanName')]",
            "type":"Microsoft.Web/serverfarms",
            "location":"[resourceGroup().location]",
            "properties":{
-              "name":"[variables('hostingPlanName')]",
-              "workerSizeId":"1",
-              "reserved":true,
-              "numberOfWorkers":"1",
-              "hostingEnvironment":""
+               "name":"[variables('hostingPlanName')]",
+               "workerTierName":null,
+               "adminSiteName":null,
+               "reserved":true,
+               "kind":"linux",
+               "perSiteScaling":false
            },
            "sku":{
               "Tier":"${var.webapp_sku_tier}",
               "Name":"${var.webapp_sku_name}",
               "capacity": "${var.notification_capacity}"
-           },
-           "kind":"linux"
+           }
         },
         {
            "type":"Microsoft.Web/sites/config",
            "name":"[concat(parameters('siteName'), '/web')]",
-           "apiVersion":"2016-08-01",
-           "location":"UK South",
-           "scale":null,
+           "apiVersion":"2019-08-01",
+           "location":"[resourceGroup().location]",
            "properties":{
               "httpLoggingEnabled":true,
               "logsDirectorySizeLimit":35,
               "detailedErrorLoggingEnabled":true,
               "alwaysOn":true,
-              "appCommandLine":"",
               "linuxFxVersion":"DOCKER|${var.docker_registry_server_url}/${var.docker_image}:${var.docker_tag}",
-              "minTlsVersion":"1.0",
+              "minTlsVersion":"1.2",
               "ftpsState":"Disabled"
            },
            "dependsOn":[
