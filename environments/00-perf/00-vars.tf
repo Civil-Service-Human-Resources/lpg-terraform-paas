99df2923965e3401a16350a965be32263b6cdb62
Pritpal <pritpalp@kainos.com>
2019-11-27 14:08:54 +0000

    Updated to add new MySQL instance for transitory data and updated container versions to match existing perf changes

--- environments/00-perf/00-vars.tf
+++ environments/00-perf/00-vars.tf
@@ -144,7 +144,7 @@ variable "lpg_services_tag" {
 }
 
 variable "notification_service_tag" {
-  default = "PR-2-7"
+  default = "master-5"
 }
 
 ### cosmos ###
@@ -270,6 +270,14 @@ variable "mysql_name_gp" {
   default = "gp"
 }
 
+### mysql_ll ###
+variable "mysql_name_ll" {
+  default = "ll"
+}
+
+variable "mysql_storage" {
+  default = "4194304"
+}
 ### civil-servant-registry ###
 variable "civil_servant_registry_name" {
   default = "civil-servant-registry"
