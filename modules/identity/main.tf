43e174041efca69b865086dd2961f1a5ddfdc546
Pritpal <pritpalp@kainos.com>
2019-11-29 14:29:26 +0000

    Updating start commands to load appinsights jar

--- modules/identity/main.tf
+++ modules/identity/main.tf
@@ -240,6 +240,7 @@ resource "azurerm_template_deployment" "identity-app-service" {
                   "logsDirectorySizeLimit":35,
                   "detailedErrorLoggingEnabled":true,
                   "alwaysOn":true,
+                  "appCommandLine": "java  -javaagent:/opt/appinsights/applicationinsights-agent-2.5.0.jar -jar /data/app.jar",
                   "linuxFxVersion":"DOCKER|${var.docker_registry_server_url}/${var.docker_image}:${var.docker_tag}",
                   "minTlsVersion":"1.2",
                   "ftpsState":"Disabled"
