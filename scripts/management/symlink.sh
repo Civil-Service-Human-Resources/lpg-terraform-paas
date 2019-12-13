c8a0ce39004cbad3ffaeb66f4c8bb04733c3b0fa
Rob Marks <robertm@kainos.com>
2019-12-13 15:18:52 +0000

    Update mongo_url
    Remove `&connectTimeoutMS=200000` from mongo_url for xapi

--- scripts/management/symlink.sh
+++ scripts/management/symlink.sh
@@ -11,6 +11,17 @@ fi
 
 if [[ "$ENV" =~ ^(production|staging|integration|perf)$ ]]; then
     echo "Setting symlink: $1"
+elif [[ "$ENV" == "unlink" ]]; then
+    echo "Removing .terraform state directory"
+    rm -rf .terraform
+
+    echo "Un-linking any symlink files"
+    unlink state.tf
+    unlink 00-vars.tf
+    unlink *-vars.tf
+    
+    echo "Environment unlinked - exiting"
+    exit 1
 else
     echo "$ENV is not a valid environment"
     exit
@@ -28,5 +39,8 @@ echo "Creating symlink for $ENV files"
 ln -s ../00-$ENV/00-vars.tf
 ln -s /keybase/team/lpg/ops/azure/cabinet-azure/00-$ENV/state.tf
 ln -s /keybase/team/lpg/ops/azure/cabinet-azure/00-$ENV/$ENV-vars.tf
+
+echo "Initialising Terraform"
+terraform init
 
 echo "Complete"
