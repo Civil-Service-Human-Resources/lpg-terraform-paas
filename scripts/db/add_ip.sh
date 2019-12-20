9938d9f9eb3fc09005479dd19504deb79cc62f8b
Rob Marks <robertm@kainos.com>
2019-12-20 14:43:29 +0000

    Update firewall script

--- scripts/db/add_ip.sh
+++ scripts/db/add_ip.sh
@@ -1,31 +1,60 @@
 #!/bin/bash -ev
-
-# This script should get a list of all outbound ip's and add them to the mysql instance firewall rules
-# It may take a while (few minutes) for the changes to actually take effect in Azure
-# Assumes that you are logged in via Azure CLI and have it installed
+#
+# Script to update the firewalls 
+# on mysql and redis instances 
 #
 # Inputs:
 # Resource group
-# Web App for Container name
 #
-# Example: ./add_ip.sh lpgtest lpg-lpgtest-wso2
+# Example: ./add_ip.sh lpgtest
 
-IN=$(az webapp show -g $1 -n $2 --query possibleOutboundIpAddresses -o tsv)
-existingMySQL=$(az mysql server firewall-rule list -g $1 -s lpg-$1-mysql | grep -i startipaddress)
-existingRedis=$(az redis firewall-rules list -g $1 -n lpg-$1-redis-session | grep -i startIp)
+identityApp="lpg-$1-identity"
+transchiverApp="lpg-$1-lpg-data-transchriver"
 
-ipList=$(echo ${IN} | tr "," "\n")
-az webapp show -g $1 -n $2 --query possibleOutboundIpAddresses -o tsv
+gpMysql="lpg-$1-gp"
+llMysql="lpg-$1-ll"
+sessionRedis="lpg-$1-redis-session"
 
-for i in ${ipList}
+SERVICES_TO_UPDATE="${gpMysql} ${llMysql} ${sessionRedis}"
+
+identityOutboundIPs=$(az webapp show -g $1 -n ${identityApp} --query possibleOutboundIpAddresses -o tsv)
+transchiverOutboundIPs=$(az webapp show -g $1 -n ${transchiverApp} --query possibleOutboundIpAddresses -o tsv)
+
+identityIPList=$(echo ${identityOutboundIPs} | tr "," "\n")
+transchiverIPList=$(echo ${transchiverOutboundIPs} | tr "," "\n")
+
+for SERVICE in ${SERVICES_TO_UPDATE}
 do
-  # do something here
-  if [[ ${existingMySQL} != *"${i}"* ]]; then
-    echo "Adding rule for ${i}"
-    addMySQLRule=$(az mysql server firewall-rule create -g $1 -s lpg-$1-gp --name Rule_${1}_${i//./} --start-ip-address ${i} --end-ip-address ${i})
-  fi
-  if [[ ${existingRedis} != *"${i}"* ]]; then
-    echo "Adding rule to Redis for ${i}"
-    addRedisRule=$(az redis firewall-rules create -g $1 -n lpg-$1-redis-session --rule-name Rule_${1}_${i//./} --start-ip ${i} --end-ip ${i})
-  fi
+    case $SERVICE in
+        ${gpMysql})
+            gpFirewallList=$(az mysql server firewall-rule list -g $1 -s ${gpMysql} | grep -i startipaddress)
+            for ip in ${identityIPList}
+            do
+                if [[ ${gpFirewallList} != *"${ip}"* ]]; then
+                    echo "Adding rule for ${ip}"
+                    addRule=$(az mysql server firewall-rule create -g $1 -s ${gpMysql} --name Rule_${1}_${ip//./} --start-ip-address ${ip} --end-ip-address ${ip})
+                fi
+            done
+            ;;
+        ${llMysql})
+            llFirewallList=$(az mysql server firewall-rule list -g $1 -s ${llMysql} | grep -i startipaddress)
+            for ip in ${transchiverIPList}
+            do
+                if [[ ${llFirewallList} != *"${ip}"* ]]; then
+                    echo "Adding rule for ${ip}"
+                    addRule=$(az mysql server firewall-rule create -g $1 -s ${llMysql} --name Rule_${1}_${ip//./} --start-ip-address ${ip} --end-ip-address ${ip})
+                fi
+            done
+            ;;
+        ${sessionRedis})
+            sessionFirewallList=$(az redis firewall-rules list -g $1 -n ${sessionRedis} | grep -i startIp)
+            for ip in ${identityIPList}
+            do
+                if [[ ${sessionFirewallList} != *"${ip}"* ]]; then
+                    echo "Adding rule to Redis for ${ip}"
+                    addRedisRule=$(az redis firewall-rules create -g $1 -n ${sessionRedis} --rule-name Rule_${1}_${ip//./} --start-ip ${ip} --end-ip ${ip})
+                fi
+            done
+            ;;
+    esac
 done
