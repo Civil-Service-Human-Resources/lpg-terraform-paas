dfa6bf64fcfa867e75d5db86c2f28c39b5dfac60
Pritpal <pritpalp@kainos.com>
2020-08-05 12:56:16 +0100

    New skills images

--- environments/00-staging/00-vars.tf
+++ environments/00-staging/00-vars.tf
@@ -131,7 +131,7 @@ variable "google_analytics_id" {
 
 ### docker tags ###
 variable "civil_servant_registry_docker_tag" {
-  default = "master-17"
+  default = "PR-123-11"
 }
 
 variable "identity_docker_tag" {
@@ -151,11 +151,11 @@ variable "lpg_learner_record_docker_tag" {
 }
 
 variable "learning_catalogue_docker_tag" {
-  default = "master-11"
+  default = "PR-137-1"
 }
 
 variable "lpg_management_tag" {
-  default = "master-18"
+  default = "PR-271-2"
 }
 
 variable "lpg_report_service_docker_tag" {
@@ -163,7 +163,7 @@ variable "lpg_report_service_docker_tag" {
 }
 
 variable "lpg_services_tag" {
-  default = "master-33"
+  default = "bugfix-removedOrgIdFromUrlCalls-3"
 }
 
 variable "notification_service_tag" {
