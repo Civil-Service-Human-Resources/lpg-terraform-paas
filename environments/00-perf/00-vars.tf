e71105a49cc42ab19a0c6c1c969a884de8740f73
Matt Doherty <matthew.doherty@kainos.com>
2020-08-18 14:39:36 +0100

    [lc-607] - Update CSRS image

--- environments/00-perf/00-vars.tf
+++ environments/00-perf/00-vars.tf
@@ -132,7 +132,7 @@ variable "google_analytics_id" {
 
 ### docker tags ###
 variable "civil_servant_registry_docker_tag" {
-  default = "master-17"
+  default = "PR-125-11"
 }
 
 variable "identity_docker_tag" {
