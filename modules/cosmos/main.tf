a4b9bcb893d9a5fd344155f45172e614faa1b109
Pritpal <pritpalp@kainos.com>
2020-09-14 12:03:51 +0100

    Updated the options needed for Cosmos to stay as it is and take into account the new options

--- modules/cosmos/main.tf
+++ modules/cosmos/main.tf
@@ -8,13 +8,28 @@ resource "azurerm_cosmosdb_account" "test" {
   offer_type          = var.cosmos_offer_type
   kind                = "MongoDB"
 
-### ****CAREFUL**** - CAPABILITIES must only be run against Staging ###
-/*
-  capabilities {
+### ****CAREFUL**** ###
+#
+# This is now a little more complex.
+# Please read carefully and comment/uncomment with care
+# Always run "plan" and check before running an "apply"
+#
+# EnableAggregationPipeline - Only needs to be uncommented for STAGE
+/*   capabilities {
     
     name = "EnableAggregationPipeline"
-  }  
-*/ 
+  }   */
+
+# AllowSelfServeUpgradeToMongo36 - Comment out fot STAGE ONLY
+  capabilities {
+    name = "AllowSelfServeUpgradeToMongo36"
+  }
+
+# DisableRateLimitingResponses - Comment out fot STAGE ONLY
+  capabilities {
+    name = "DisableRateLimitingResponses"
+  }
+
 ### ^^^^CAREFUL^^^^ ###
 #
   consistency_policy {
