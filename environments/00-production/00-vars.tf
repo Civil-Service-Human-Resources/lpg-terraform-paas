a0868c08a0c83120448fce8a05cac2577ca02373
Jain Manglesh <manglesh.jain@cabinetoffice.gov.uk>
2020-12-14 12:58:16 +0000

    updated tag for lpg-ui

--- environments/00-production/00-vars.tf
+++ environments/00-production/00-vars.tf
@@ -164,7 +164,7 @@ variable "lpg_report_service_docker_tag" {
 }
 
 variable "lpg_services_tag" {
-  default = "idt-release-1.0-1"
+  default = "idt-release-1.0-2"
 }
 
 variable "notification_service_tag" {
