470430b909d68bee11b0356aec5df01f079eec72
Jain Neeraj <neeraj.jain@cabinetoffice.gov.uk>
2021-02-05 12:22:15 +0000

    LC-845 config based maintenance page

--- environments/master/main.tf
+++ environments/master/main.tf
@@ -109,6 +109,11 @@ module "identity" {
   jwt_key                                 = var.jwt_key
   invite_validity                         = var.invite_validity
   duration_after_rereg_allowed_seconds    = var.duration_after_rereg_allowed_seconds
+  maintenance_page_enabled                = var.maintenance_page_enabled
+  maintenance_page_content_line_1         = var.maintenance_page_content_line_1
+  maintenance_page_content_line_2         = var.maintenance_page_content_line_2
+  maintenance_page_content_line_3         = var.maintenance_page_content_line_3
+  maintenance_page_content_line_4         = var.maintenance_page_content_line_4
 }
 
 module "identity-management" {
