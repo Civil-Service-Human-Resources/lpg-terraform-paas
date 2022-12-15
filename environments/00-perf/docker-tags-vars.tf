4627ea5fb1c145fd563a49f6eba26e248f6d2295
jaylco <77386599+jaylco@users.noreply.github.com>
2022-12-15 14:09:27 +0000

    corrected tag on lpg-management

--- environments/00-perf/docker-tags-vars.tf
+++ environments/00-perf/docker-tags-vars.tf
@@ -11,7 +11,7 @@ variable "civil_servant_registry_docker_repository_region" {
 ## identity ##
 
 variable "identity_docker_tag" {
-  default = "idt-release-v1.4.0"
+  default = "idt-release-v1.5.0"
 }
 
 variable "identity_docker_repository_region" {
@@ -21,7 +21,7 @@ variable "identity_docker_repository_region" {
 ## identity-management ##
 
 variable "identity_management_docker_tag" {
-  default = "idt-release-v1.5.0"
+  default = "master"
 }
 
 variable "identity_management_docker_repository_region" {
@@ -57,7 +57,7 @@ variable "learning_catalogue_docker_repository_region" {
 ## lpg-management ##
 
 variable "lpg_management_tag" {
-  default = "idt-release-v1.3.0"
+  default = "idt-release-v1.5.0"
 }
 
 variable "lpg_management_docker_repository_region" {
