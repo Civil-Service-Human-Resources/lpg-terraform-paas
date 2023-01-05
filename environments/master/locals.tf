6b4e48837af487884c959505a9ed12c17df0b545
jaylco <77386599+jaylco@users.noreply.github.com>
2023-01-05 11:40:26 +0000

    new app service data resource

--- environments/master/locals.tf
+++ environments/master/locals.tf
@@ -1,52 +1,52 @@
-data "azurerm_app_service" "lpg_ui_data" {
+data "azurerm_linux_web_app" "lpg_ui_data" {
 	name = "${var.rg_prefix}-${var.rg_name}-${var.lpg_ui_name}"
 	resource_group_name = var.rg_name_lpg_ui
 }
 
-data "azurerm_app_service" "lpg_management_data" {
+data "azurerm_linux_web_app" "lpg_management_data" {
 	name = "${var.rg_prefix}-${var.rg_name}-${var.lpg_management_name}"
 	resource_group_name = var.rg_name
 }
 
-data "azurerm_app_service" "report_service_data" {
+data "azurerm_linux_web_app" "report_service_data" {
 	name = "${var.rg_prefix}-${var.rg_name}-${var.lpg_report_service_name}"
 	resource_group_name = var.rg_name
 }
 
-data "azurerm_app_service" "identity_data" {
+data "azurerm_linux_web_app" "identity_data" {
 	name = "${var.rg_prefix}-${var.rg_name}-${var.identity_name}"
 	resource_group_name = var.rg_name
 }
 
-data "azurerm_app_service" "civil_servant_registry_data" {
+data "azurerm_linux_web_app" "civil_servant_registry_data" {
 	name = "${var.rg_prefix}-${var.rg_name}-${var.civil_servant_registry_name}"
 	resource_group_name = var.rg_name
 }
 
-data "azurerm_app_service" "learning_catalogue_data" {
+data "azurerm_linux_web_app" "learning_catalogue_data" {
 	name = "${var.rg_prefix}-${var.rg_name}-${var.lpg_learning_catalogue_name}"
 	resource_group_name = var.rg_name
 }
 
-data "azurerm_app_service" "learner_record_data" {
+data "azurerm_linux_web_app" "learner_record_data" {
 	name = "${var.rg_prefix}-${var.rg_name}-${var.lpg_learner_record_name}"
 	resource_group_name = var.rg_name
 }
 
-data "azurerm_app_service" "identity_management_data" {
+data "azurerm_linux_web_app" "identity_management_data" {
 	name = "${var.rg_prefix}-${var.rg_name}-${var.identity_management_name}"
 	resource_group_name = var.rg_name
 }
 
 locals {
-  	lpg_ui_ips = data.azurerm_app_service.lpg_ui_data.possible_outbound_ip_address_list
-	lpg_management_ips = data.azurerm_app_service.lpg_management_data.possible_outbound_ip_address_list
-    lpg_report_service_ips = data.azurerm_app_service.report_service_data.possible_outbound_ip_address_list
-    identity_ips = data.azurerm_app_service.identity_data.possible_outbound_ip_address_list
-    civil_servant_registry_ips = data.azurerm_app_service.civil_servant_registry_data.possible_outbound_ip_address_list
-    learning_catalogue_ips = data.azurerm_app_service.learning_catalogue_data.possible_outbound_ip_address_list
-    learner_record_ips = data.azurerm_app_service.learner_record_data.possible_outbound_ip_address_list
-    identity_management_ips = data.azurerm_app_service.identity_management_data.possible_outbound_ip_address_list
+  	lpg_ui_ips = data.azurerm_linux_web_app.lpg_ui_data.possible_outbound_ip_address_list
+	lpg_management_ips = data.azurerm_linux_web_app.lpg_management_data.possible_outbound_ip_address_list
+    lpg_report_service_ips = data.azurerm_linux_web_app.report_service_data.possible_outbound_ip_address_list
+    identity_ips = data.azurerm_linux_web_app.identity_data.possible_outbound_ip_address_list
+    civil_servant_registry_ips = data.azurerm_linux_web_app.civil_servant_registry_data.possible_outbound_ip_address_list
+    learning_catalogue_ips = data.azurerm_linux_web_app.learning_catalogue_data.possible_outbound_ip_address_list
+    learner_record_ips = data.azurerm_linux_web_app.learner_record_data.possible_outbound_ip_address_list
+    identity_management_ips = data.azurerm_linux_web_app.identity_management_data.possible_outbound_ip_address_list
 	allowed_ips = toset(concat(
 		local.lpg_ui_ips,
 		local.lpg_management_ips,
