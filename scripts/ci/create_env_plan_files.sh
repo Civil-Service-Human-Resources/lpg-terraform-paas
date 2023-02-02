2fbda6df8dc63d05af7b27c67e08e5432b7fbc69
SavvasCO <savvas.stephanides@cabinetoffice.gov.uk>
2023-02-02 10:08:14 +0000

    Azure pipeline

--- scripts/ci/create_env_plan_files.sh
+++ scripts/ci/create_env_plan_files.sh
@@ -1,8 +1,17 @@
 rm -rf plan-files
 mkdir plan-files
 
-for ENVIRONMENT in integration staging
+for ENVIRONMENT in integration staging perf production
 do
+    var_name=${ENVIRONMENT}_MYSQL_USER
+    export TF_VAR_mysql_user=${!var_name}
+
+    var_name=${ENVIRONMENT}_MYSQL_PASS
+    export TF_VAR_mysql_pass=${!var_name}
+
+    var_name=${ENVIRONMENT}_MYSQL_PASS_LL
+    export TF_VAR_mysql_pass_ll=${!var_name}
+    
     ./scripts/management/plan.sh $ENVIRONMENT
     mv ./plan/$ENVIRONMENT/tfplan-$ENVIRONMENT ./plan-files/.
 done
--- scripts/ci/validate_envs.sh
+++ scripts/ci/validate_envs.sh
@@ -5,6 +5,23 @@ mkdir plan
 
 for ENVIRONMENT in integration staging perf production
 do
+    var_name=${ENVIRONMENT}_MYSQL_USER
+    export TF_VAR_mysql_user=${!var_name}
+
+    var_name=${ENVIRONMENT}_MYSQL_PASS
+    export TF_VAR_mysql_pass=${!var_name}
+
+    var_name=${ENVIRONMENT}_MYSQL_PASS_LL
+    export TF_VAR_mysql_pass_ll=${!var_name}
+    
+    if [[ $ENVIRONMENT =~ ^(integration|staging|perf)$ ]]; then
+        export SUBSCRIPTION_NAME="CSL-Staging"
+        export TF_VAR_${ENVIRONMENT}_subscription_id=$STAGING_SUBSCRIPTION_ID
+    else
+        export SUBSCRIPTION_NAME="CSL-Production"
+        export TF_VAR_production_subscription_id=$PRODUCTION_SUBSCRIPTION_ID
+    fi
+
     cd $ROOT/plan
     mkdir $ENVIRONMENT
     cd $ENVIRONMENT
@@ -17,15 +34,10 @@ do
     cp -r ../../environments/$ENVIRONMENT/state.tf .
     cp -r ../../environments/$ENVIRONMENT/vars.tf $ENVIRONMENT-vars.tf
 
-    if [[ $ENVIRONMENT =~ ^(integration|staging|perf)$ ]]; then
-        SUBSCRIPTION_NAME="CSL-Staging"
-    else
-        SUBSCRIPTION_NAME="CSL-Production"
-    fi
-
     az account set --subscription $SUBSCRIPTION_NAME
 
-    echo """access_key=\"$access_key\"""" > backend.conf
+    var_name=${ENVIRONMENT}_ACCESS_KEY
+    echo "access_key=\"${!var_name}\"" > backend.conf
 
     terraform init -backend-config=backend.conf
     terraform validate
